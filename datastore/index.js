21550a3156bd31e48a45f66b26dac0f83db5b87b
Grant Taggart <granttaggartsf@gmail.com>
2021-01-07 17:13:45 -0800

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -51,7 +51,6 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-
   fs.readFile(path.join(exports.dataDir, `${id}.txt`), (err) => {
     if (err) {
       callback(new Error(`No item with id: ${id}`));
@@ -68,14 +67,13 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  fs.unlink(path.join(exports.dataDir, `${id}.txt`), (err) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback();
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
