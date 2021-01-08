c90a77123fa4c2e83ad118cd6fdd60167951d08e
Grant Taggart <granttaggartsf@gmail.com>
2021-01-07 17:06:34 -0800

    Complete updating a todo

--- datastore/index.js
+++ datastore/index.js
@@ -51,13 +51,20 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+
+  fs.readFile(path.join(exports.dataDir, `${id}.txt`), (err) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      fs.writeFile(path.join(exports.dataDir, `${id}.txt`), text, (err) => {
+        if (err) {
+          callback(new Error('No dir at: dataDir'));
+        } else {
+          callback(null, { id, text });
+        }
+      });
+    }
+  });
 };
 
 exports.delete = (id, callback) => {
